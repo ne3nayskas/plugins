7645611d36d49653e6965456c478eeadb4725dd6
ne3nayskas <55268575+ne3nayskas@users.noreply.github.com>
2025-09-17 19:11:16 +0300

    Update hide_menu.js

--- hide_menu.js
+++ hide_menu.js
@@ -3,20 +3,20 @@
   
     var plugin_name = 'hide_menu_items';  
     var settings = {  
-        // Прибрали hide_main та hide_other  
-        hide_sport: true,  
-        hide_timeline: true,      // Стрічка  
-        hide_torrents: true,      // Торренти  
-        hide_schedule: true,      // Розклад  
-        hide_subscriptions: true, // Підписки  
-        hide_bookmarks: true,     // Вибране  
-        hide_anime: true,         // Аніме  
-        hide_catalog: true,       // Каталог  
-        hide_persons: true,       // Особи  
-        hide_info: true,          // Інформація  
-        hide_console: true,       // Консоль  
-        hide_settings: true,      // Налаштування  
-        hide_filter: true         // Фільтр  
+        // За замовчуванням всі пункти НЕ приховані  
+        hide_sport: false,  
+        hide_timeline: false,      // Стрічка  
+        hide_torrents: false,      // Торренти  
+        hide_schedule: false,      // Розклад  
+        hide_subscriptions: false, // Підписки  
+        hide_bookmarks: false,     // Вибране  
+        hide_anime: false,         // Аніме  
+        hide_catalog: false,       // Каталог  
+        hide_persons: false,       // Особи  
+        hide_info: false,          // Інформація  
+        hide_console: false,       // Консоль  
+        hide_settings: false,      // Налаштування (тільки в лівому меню)  
+        hide_filter: false         // Фільтр  
     };  
   
     function loadSettings() {  
@@ -33,36 +33,144 @@
   
     function hideMenuItems() {  
         var menuSelectors = {  
-            'sport': settings.hide_sport,  
-            'timeline': settings.hide_timeline,  
-            'torrents': settings.hide_torrents,  
-            'schedule': settings.hide_schedule,  
-            'subscriptions': settings.hide_subscriptions,  
-            'bookmarks': settings.hide_bookmarks,  
-            'anime': settings.hide_anime,  
-            'catalog': settings.hide_catalog,  
-            'persons': settings.hide_persons,  
-            'info': settings.hide_info,  
-            'console': settings.hide_console,  
-            'settings': settings.hide_settings,  
-            'filter': settings.hide_filter  
+            'sport': {  
+                enabled: settings.hide_sport,  
+                selectors: [  
+                    '.menu__item[data-action="sport"]',  
+                    '.sidebar__item[data-action="sport"]',  
+                    '[data-action="sport"]:not(.head__action)',  
+                    '.navigation__item[data-action="sport"]'  
+                ]  
+            },  
+            'timeline': {  
+                enabled: settings.hide_timeline,  
+                selectors: [  
+                    '.menu__item[data-action="timeline"]',  
+                    '.sidebar__item[data-action="timeline"]',  
+                    '[data-action="timeline"]:not(.head__action)',  
+                    '.navigation__item[data-action="timeline"]'  
+                ]  
+            },  
+            'torrents': {  
+                enabled: settings.hide_torrents,  
+                selectors: [  
+                    '.menu__item[data-action="torrents"]',  
+                    '.sidebar__item[data-action="torrents"]',  
+                    '[data-action="torrents"]:not(.head__action)',  
+                    '.navigation__item[data-action="torrents"]'  
+                ]  
+            },  
+            'schedule': {  
+                enabled: settings.hide_schedule,  
+                selectors: [  
+                    '.menu__item[data-action="schedule"]',  
+                    '.sidebar__item[data-action="schedule"]',  
+                    '[data-action="schedule"]:not(.head__action)',  
+                    '.navigation__item[data-action="schedule"]'  
+                ]  
+            },  
+            'subscriptions': {  
+                enabled: settings.hide_subscriptions,  
+                selectors: [  
+                    '.menu__item[data-action="subscriptions"]',  
+                    '.sidebar__item[data-action="subscriptions"]',  
+                    '[data-action="subscriptions"]:not(.head__action)',  
+                    '.navigation__item[data-action="subscriptions"]'  
+                ]  
+            },  
+            'bookmarks': {  
+                enabled: settings.hide_bookmarks,  
+                selectors: [  
+                    '.menu__item[data-action="bookmarks"]',  
+                    '.sidebar__item[data-action="bookmarks"]',  
+                    '[data-action="bookmarks"]:not(.head__action)',  
+                    '.navigation__item[data-action="bookmarks"]'  
+                ]  
+            },  
+            'anime': {  
+                enabled: settings.hide_anime,  
+                selectors: [  
+                    '.menu__item[data-action="anime"]',  
+                    '.sidebar__item[data-action="anime"]',  
+                    '[data-action="anime"]:not(.head__action)',  
+                    '.navigation__item[data-action="anime"]'  
+                ]  
+            },  
+            'catalog': {  
+                enabled: settings.hide_catalog,  
+                selectors: [  
+                    '.menu__item[data-action="catalog"]',  
+                    '.sidebar__item[data-action="catalog"]',  
+                    '[data-action="catalog"]:not(.head__action)',  
+                    '.navigation__item[data-action="catalog"]'  
+                ]  
+            },  
+            'persons': {  
+                enabled: settings.hide_persons,  
+                selectors: [  
+                    '.menu__item[data-action="persons"]',  
+                    '.sidebar__item[data-action="persons"]',  
+                    '[data-action="persons"]:not(.head__action)',  
+                    '.navigation__item[data-action="persons"]'  
+                ]  
+            },  
+            'info': {  
+                enabled: settings.hide_info,  
+                selectors: [  
+                    '.menu__item[data-action="info"]',  
+                    '.sidebar__item[data-action="info"]',  
+                    '[data-action="info"]:not(.head__action)',  
+                    '.navigation__item[data-action="info"]',  
+                    '.menu__item[data-component="info"]',  
+                    '.sidebar__item[data-component="info"]'  
+                ]  
+            },  
+            'console': {  
+                enabled: settings.hide_console,  
+                selectors: [  
+                    '.menu__item[data-action="console"]',  
+                    '.sidebar__item[data-action="console"]',  
+                    '[data-action="console"]:not(.head__action)',  
+                    '.navigation__item[data-action="console"]'  
+                ]  
+            },  
+            'settings': {  
+                enabled: settings.hide_settings,  
+                selectors: [  
+                    // Приховуємо тільки в лівому меню, НЕ в шапці  
+                    '.menu__item[data-action="settings"]',  
+                    '.sidebar__item[data-action="settings"]',  
+                    '.navigation__item[data-action="settings"]'  
+                    // НЕ включаємо .head__action або інші селектори шапки  
+                ]  
+            },  
+            'filter': {  
+                enabled: settings.hide_filter,  
+                selectors: [  
+                    '.menu__item[data-action="filter"]',  
+                    '.sidebar__item[data-action="filter"]',  
+                    '[data-action="filter"]:not(.head__action)',  
+                    '.navigation__item[data-action="filter"]'  
+                ]  
+            }  
         };  
   
         Object.keys(menuSelectors).forEach(function(key) {  
-            if (menuSelectors[key]) {  
-                // Пробуємо різні селектори для знаходження елементів меню  
-                var selectors = [  
-                    '[data-action="' + key + '"]',  
-                    '[data-component="' + key + '"]',  
-                    '.menu__item[data-action="' + key + '"]',  
-                    '.sidebar__item[data-action="' + key + '"]'  
-                ];  
-                  
-                selectors.forEach(function(selector) {  
-                    var elements = document.querySelectorAll(selector);  
-                    elements.forEach(function(element) {  
-                        element.style.display = 'none';  
-                    });  
+            var config = menuSelectors[key];  
+            if (config.enabled) {  
+                config.selectors.forEach(function(selector) {  
+                    try {  
+                        var elements = document.querySelectorAll(selector);  
+                        elements.forEach(function(element) {  
+                            // Перевіряємо чи елемент існує та чи він видимий  
+                            if (element && element.offsetParent !== null) {  
+                                element.style.display = 'none';  
+                            }  
+                        });  
+                    } catch(e) {  
+                        // Ігноруємо помилки селекторів  
+                        console.warn('Selector error for ' + key + ':', e);  
+                    }  
                 });  
             }  
         });  
@@ -88,7 +196,7 @@
             { key: 'hide_persons', title: 'Приховати Особи' },  
             { key: 'hide_info', title: 'Приховати Інформація' },  
             { key: 'hide_console', title: 'Приховати Консоль' },  
-            { key: 'hide_settings', title: 'Приховати Налаштування' },  
+            { key: 'hide_settings', title: 'Приховати Налаштування (тільки в меню)' },  
             { key: 'hide_filter', title: 'Приховати Фільтр' }  
         ];  
   
@@ -112,24 +220,41 @@
             });  
         });  
   
-        // Додаємо кнопку для швидкого скидання налаштувань  
+        // Додаємо кнопку для показу всіх пунктів  
         Lampa.SettingsApi.addParam({  
             component: 'hide_menu_plugin',  
             param: {  
-                name: plugin_name + '_reset',  
+                name: plugin_name + '_show_all',  
                 type: 'button'  
             },  
             field: {  
-                name: 'Скинути всі налаштування',  
-                description: 'Показати всі пункти меню'  
+                name: 'Показати всі пункти меню',  
+                description: 'Вимкнути приховування всіх пунктів'  
             },  
             onChange: function() {  
                 Object.keys(settings).forEach(function(key) {  
                     settings[key] = false;  
                 });  
                 saveSettings();  
-                location.reload();  
+                // Показуємо всі елементи  
+                showAllMenuItems();  
             }  
+        });  
+    }  
+  
+    function showAllMenuItems() {  
+        // Показуємо всі приховані елементи  
+        var allSelectors = [  
+            '.menu__item', '.sidebar__item', '.navigation__item'  
+        ];  
+          
+        allSelectors.forEach(function(selector) {  
+            var elements = document.querySelectorAll(selector);  
+            elements.forEach(function(element) {  
+                if (element.style.display === 'none') {  
+                    element.style.display = '';  
+                }  
+            });  
         });  
     }  
   
@@ -141,7 +266,6 @@
         Lampa.Listener.follow('app', function(e) {  
             if (e.type == 'ready') {  
                 setTimeout(hideMenuItems, 1000);  
-                // Повторюємо через деякий час для надійності  
                 setTimeout(hideMenuItems, 3000);  
             }  
         });  
@@ -159,6 +283,27 @@
                 setTimeout(hideMenuItems, 200);  
             }  
         });  
+  
+        // Застосування при зміні DOM  
+        var observer = new MutationObserver(function(mutations) {  
+            var shouldUpdate = false;  
+            mutations.forEach(function(mutation) {  
+                if (mutation.type === 'childList' && mutation.addedNodes.length > 0) {  
+                    shouldUpdate = true;  
+                }  
+            });  
+            if (shouldUpdate) {  
+                setTimeout(hideMenuItems, 100);  
+            }  
+        });  
+  
+        // Спостерігаємо за змінами в DOM  
+        if (document.body) {  
+            observer.observe(document.body, {  
+                childList: true,  
+                subtree: true  
+            });  
+        }  
     }  
   
     // Запуск плагіна  
