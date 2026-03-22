8d21d189fb1f133bba5ae812772b11b61b600d4f
ne3nayskas <55268575+ne3nayskas@users.noreply.github.com>
2026-03-22 12:31:09 +0200

    Refactor TMDB top plugin and menu button logic
    Refactor TMDB top plugin to load top movies and update menu integration.

--- top.js
+++ top.js
@@ -1,6 +1,4 @@
 // plugins/tmdb-top/tmdb-top.js  
-import Lang from './lang.js'  
-  
 function startPlugin() {  
     window.plugin_tmdb_top_ready = true  
   
@@ -21,66 +19,49 @@ function startPlugin() {
             uk: 'Топ-100 фільмів',  
             en: 'Top 100 Movies',  
             be: 'Топ-100 фільмаў'  
-        },  
-        tmdb_top_loading: {  
-            ru: 'Загрузка...',  
-            uk: 'Завантаження...',  
-            en: 'Loading...',  
-            be: 'Загрузка...'  
-        },  
-        tmdb_top_error: {  
-            ru: 'Ошибка загрузки',  
-            uk: 'Помилка завантаження',  
-            en: 'Loading error',  
-            be: 'Памылка загрузкі'  
         }  
     })  
   
     // Додаємо ряд на головну сторінку  
     Lampa.ContentRows.add({  
+        name: 'tmdb_top_main',  
+        title: 'TMDB Top 100',  
         index: 2,  
         screen: ['main'],  
         call: (params, screen) => {  
+            if(Lampa.Account.Permit.child) return  
+  
             return function(call) {  
                 Lampa.TMDB.get('movie/top_rated', {  
                     page: 1,  
-                    language: Lampa.Storage.get('language', 'ru')  
+                    language: Lampa.Storage.field('tmdb_lang') || 'ru'  
                 }, (data) => {  
                     if (data.results && data.results.length > 0) {  
-                        // Обмежуємо до 100 фільмів  
-                        let movies = data.results.slice(0, 100)  
+                        let movies = data.results.slice(0, 20) // Показуємо перші 20 на головній  
                           
-                        // Розширюємо параметри для створення карток  
+                        // Використовуємо правильний метод розширення параметрів  
                         Lampa.Utils.extendItemsParams(movies, {  
                             createInstance: (item_data) => {  
-                                return Lampa.Card(item_data, {  
-                                    cardClass: 'movie',  
-                                    object: {  
-                                        title: item_data.title,  
-                                        original_title: item_data.original_title,  
-                                        release_date: item_data.release_date,  
-                                        vote_average: item_data.vote_average,  
-                                        poster: item_data.poster_path,  
-                                        backdrop: item_data.backdrop_path  
-                                    }  
-                                })  
+                                return Lampa.Maker.make('Card', item_data, (module) => module.only('Card', 'Callback'))  
                             },  
                             onMore: () => {  
                                 Lampa.Activity.push({  
-                                    url: '',  
+                                    url: 'movie/top_rated',  
                                     title: Lampa.Lang.translate('tmdb_top_title'),  
                                     component: 'category',  
-                                    genre: 'top_rated',  
                                     page: 1  
                                 })  
                             }  
                         })  
   
                         call({  
-                            results: movies,  
                             title: Lampa.Lang.translate('tmdb_top_title'),  
+                            results: movies,  
                             type: 'movie',  
                             total_pages: Math.ceil(Math.min(data.total_results, 100) / 20),  
+                            icon_svg: '<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z" fill="currentColor"/></svg>',  
+                            icon_bgcolor: '#fff',  
+                            icon_color: '#fd4518',  
                             params: {  
                                 module: Lampa.Maker.module('Line').toggle(Lampa.Maker.module('Line').MASK.base, 'Icon')  
                             }  
@@ -103,41 +84,6 @@ function startPlugin() {
             }  
         }  
     })  
-  
-    // Додаємо кнопку в меню  
-    function addMenuButton() {  
-        let button = $(`<li class="menu__item selector">  
-            <div class="menu__ico">  
-                <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">  
-                    <path d="M12 2L15.09 8.26L22 9.27L17 14.14L18.18 21.02L12 17.77L5.82 21.02L7 14.14L2 9.27L8.91 8.26L12 2Z" fill="currentColor"/>  
-                </svg>  
-            </div>  
-            <div class="menu__text">${Lampa.Lang.translate('tmdb_top_title')}</div>  
-        </li>`)  
-  
-        button.on('hover:enter', function () {  
-            Lampa.Activity.push({  
-                url: '',  
-                title: Lampa.Lang.translate('tmdb_top_title'),  
-                component: 'category',  
-                genre: 'top_rated',  
-                page: 1  
-            })  
-        })  
-  
-        $('.menu .menu__list').eq(0).append(button)  
-    }  
-  
-    // Ініціалізація після готовності додатку  
-    if (window.appready) {  
-        addMenuButton()  
-    } else {  
-        Lampa.Listener.follow('app', function (e) {  
-            if (e.type == 'ready') {  
-                addMenuButton()  
-            }  
-        })  
-    }  
 }  
   
 // Запуск плагіна  
