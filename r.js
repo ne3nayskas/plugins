1ff52ce5715c3809807fe070f15724342325c1e0
ne3nayskas <55268575+ne3nayskas@users.noreply.github.com>
2026-07-12 23:22:13 +0200

    Update API URL to new endpoint

--- r.js
+++ r.js
@@ -63,7 +63,7 @@
 
   _defineProperty(Api, "network", new Lampa.Reguest());
 
-  _defineProperty(Api, "api_url", 'https://www.radiorecord.ru/api/stations/');
+  _defineProperty(Api, "api_url", 'https://openradioplayer.com/api/v1/stations/healthy');
 
   var Favorites = /*#__PURE__*/function () {
     function Favorites() {
